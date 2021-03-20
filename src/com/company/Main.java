50d31de34f004edfae26462f446bdca343b93590
rafsun-dev <rafsun.dev@gmail.com>
2021-03-20 22:11:21 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 20;
+        int y = 10;
+
+        x+=y;
+        System.out.println("x = "+x);
+
+        x-=y;
+        System.out.println("x = "+x);
+
+        x*=y;
+        System.out.println("x = "+x);
+
+        x/=y;
+        System.out.println("x = "+x);
+
+        x%=y;
+        System.out.println("x = "+x);
+    }
+}
